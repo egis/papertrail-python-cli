aa7322319028f1f23cc7fa741978b2e37b1c64bf
Nikita Baksalyar <nikita.baksalyar@gmail.com>
2016-08-13 01:24:03 +0500

    Move 'upgrade' command to plugin

--- pt/pt.py
+++ pt/pt.py
@@ -9,7 +9,6 @@ import click
 from client import Client
 from pql import print_pql_response, print_pql_csv, print_pql_json, run_pql_repl
 import service
-import version as ver
 import commands
 
 @click.group()
@@ -114,40 +113,6 @@ def form(client, url, data):
     """
     data = { pair[0]: pair[1] for pair in map(lambda pair: pair.split('='), data) }
     client.post(url, data)
-
-@papertrail.command()
-@click.argument('version', required=False)
-@click.option('--norestart', is_flag=True, help="Turn off auto restart of a Papertrail instance after the update")
-@click.option('--output', '-o', default='/opt/Papertrail.sh', help="Destination file for the upgrade package")
-def upgrade(version, norestart, output):
-    """
-    Upgrades a local Papertrail installation to the latest available version.
-
-    Special version identifiers are: stable, nightly, stable-nightly.
-    Default version is "stable".
-    """
-    if version in [None, ver.STABLE, ver.NIGHTLY, ver.STABLE_NIGHTLY]:
-        build = ver.get_build(version)
-    else:
-        build = version
-
-    # Check if the local instance needs to be upgraded
-    if build == ver.get_local_version():
-        click.echo('You are running the latest version of Papertrail (%s).' % (build))
-        return
-
-    # Download the build
-    click.echo('Downloading version %s' % (build))
-    ver.download(build, output)
-
-    # Unpack and install the downloaded package
-    click.echo('Upgrading')
-    service.upgrade(output)
-
-    ver.store_local_version(build)
-
-    if not norestart:
-        service.start()
 
 @papertrail.command(name="service")
 @click.argument('action', type=click.Choice(['start', 'stop', 'restart', 'status']))
