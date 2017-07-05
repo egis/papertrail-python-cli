741d51be1e09f5b3bf6f659ab04bf01ba8199055
Nikolay Glushchenko <nick@nickalie.com>
2017-07-05 16:13:02 +0300

    fixed versioning of the package

--- setup.py
+++ setup.py
@@ -15,9 +15,19 @@ requirements=[
 if os.name == 'posix':
     requirements.append('sh')
 
+buildNumber = os.getenv('CIRCLE_BUILD_NUM')
+
+# {{BUILD_NUMBER}} is replaced by CI.
+if buildNumber is None:
+    buildNumber = "{{BUILD_NUMBER}}"
+
+# {{BUILD_NUMBER}} wasn't replaced with proper build number (for instance, installation from git)
+if buildNumber.startswith("{{BUILD_"):
+    buildNumber = "dev"
+
 setup(
     name='papertrail-cli',
-    version='1.0.' + os.environ['CIRCLE_BUILD_NUM'],
+    version='1.0.' + buildNumber,
     install_requires=requirements,
     author='Egis Software',
     url='https://github.com/egis/papertrail-python-cli',
